8a0156b48c13ac525c700ea1c8c20ce989c07442
just-port <justinportell84@gmail.com>
2021-09-01 19:30:24 -0500

    initial commit

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,36 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
+  @Query((returns) => LessonType)
+  async lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  async lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  async createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+}
